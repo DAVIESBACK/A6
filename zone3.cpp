600f7a77a54f81eb81f850cfdc70849faec19201
Beam <b3bb0z@gmail.com>
2025-02-23 22:04:17 +0700

    adjust ui

--- zone3.cpp
+++ zone3.cpp
@@ -1,29 +1,60 @@
+#include "zone3.h"
 #include <windows.h>
 
 #define ID_BTN_HISTORY 101
 #define ID_BTN_HOME 102
 #define ID_BTN_LOGOUT 103
-#define ID_BTN_NARISA 104
-#define ID_BTN_PS 105
-#define ID_BTN_BAAN 106
-#define ID_BTN_TARNTHONG 107
-#define ID_BTN_THONGTARA 108
+#define ID_BTN_WARISA 114
+#define ID_BTN_PS 115
+#define ID_BTN_BAAAN 116
+#define ID_BTN_TARNTHONG 117
+#define ID_BTN_THONGTARA 118
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+void CreateAppButtons(HWND hwnd, HINSTANCE hInstance);
+void ResizeButtons(HWND hwnd, int width, int height);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = "HotelApp";
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    RegisterClass(&wc);
+    
+    HWND hwnd = CreateWindow("HotelApp", "Hotel Management", WS_OVERLAPPEDWINDOW | WS_VISIBLE | WS_MAXIMIZE, 
+                             CW_USEDEFAULT, CW_USEDEFAULT, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN),
+                             NULL, NULL, hInstance, NULL);
+    
+    if (!hwnd) return 0;
+    
+    ShowWindow(hwnd, nCmdShow);
+    
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return 0;
+}
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
         case WM_CREATE:
-            // Set background color
-            SetClassLongPtr(hwnd, GCL_HBRBACKGROUND, (LONG_PTR)CreateSolidBrush(RGB(240, 240, 240)));
+            CreateAppButtons(hwnd, ((LPCREATESTRUCT)lParam)->hInstance);
+            break;
+        case WM_SIZE:
+            ResizeButtons(hwnd, LOWORD(lParam), HIWORD(lParam));
             break;
         case WM_COMMAND:
             switch (LOWORD(wParam)) {
-                case ID_BTN_NARISA:
-                    MessageBox(hwnd, "Narisa Apartment Information", "Info", MB_OK | MB_ICONINFORMATION);
+                case ID_BTN_WARISA:
+                    MessageBox(hwnd, "Warisa Apartment Information", "Info", MB_OK | MB_ICONINFORMATION);
                     break;
                 case ID_BTN_PS:
                     MessageBox(hwnd, "P.S. Mansion Information", "Info", MB_OK | MB_ICONINFORMATION);
                     break;
-                case ID_BTN_BAAN:
+                case ID_BTN_BAAAN:
                     MessageBox(hwnd, "Baan Tarnkam Information", "Info", MB_OK | MB_ICONINFORMATION);
                     break;
                 case ID_BTN_TARNTHONG:
@@ -51,45 +82,50 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    WNDCLASS wc = {};
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = "HotelApp";
-    RegisterClass(&wc);
+void CreateAppButtons(HWND hwnd, HINSTANCE hInstance) {
+    HFONT hFont = CreateFont(18, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE, DEFAULT_CHARSET, 
+                             OUT_OUTLINE_PRECIS, CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, VARIABLE_PITCH, TEXT("Arial"));
     
-    HWND hwnd = CreateWindow("HotelApp", "Hotel Management", WS_OVERLAPPEDWINDOW, 
-                             CW_USEDEFAULT, CW_USEDEFAULT, 600, 500,
-                             NULL, NULL, hInstance, NULL);
+    struct ButtonInfo {
+        LPCSTR text;
+        int id;
+        HWND hWnd;
+    } buttons[] = {
+        {"Booking History", ID_BTN_HISTORY, NULL},
+        {"Warisa Apartment", ID_BTN_WARISA, NULL},
+        {"P.S. Mansion", ID_BTN_PS, NULL},
+        {"Baan Tarnkam", ID_BTN_BAAAN, NULL},
+        {"Tarnthong Place", ID_BTN_TARNTHONG, NULL},
+        {"Thongtara Monte", ID_BTN_THONGTARA, NULL},
+        {"Home", ID_BTN_HOME, NULL},
+        {"Log Out", ID_BTN_LOGOUT, NULL}
+    };
+
+    for (int i = 0; i < 8; i++) {
+        buttons[i].hWnd = CreateWindow("BUTTON", buttons[i].text, WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 
+                                       0, 0, 0, 0, hwnd, (HMENU)(UINT_PTR)buttons[i].id, hInstance, NULL);
+        SendMessage(buttons[i].hWnd, WM_SETFONT, (WPARAM)hFont, TRUE);
+    }
+}
+
+void ResizeButtons(HWND hwnd, int width, int height) {
+    int buttonWidth = width / 6;
+    int buttonHeight = 50;
+    int yOffset = 10;
     
-    if (!hwnd) return 0;
-    
-    HFONT hFont = CreateFont(18, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_OUTLINE_PRECIS, 
-                             CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, VARIABLE_PITCH, TEXT("Arial"));
-    
-    HWND buttons[] = {
-        CreateWindow("BUTTON", "Booking History", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 20, 20, 180, 40, hwnd, (HMENU)ID_BTN_HISTORY, hInstance, NULL),
-        CreateWindow("BUTTON", "Narisa Apartment", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 50, 80, 180, 40, hwnd, (HMENU)ID_BTN_NARISA, hInstance, NULL),
-        CreateWindow("BUTTON", "P.S. Mansion", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 250, 80, 180, 40, hwnd, (HMENU)ID_BTN_PS, hInstance, NULL),
-        CreateWindow("BUTTON", "Baan Tarnkam", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 150, 140, 180, 40, hwnd, (HMENU)ID_BTN_BAAN, hInstance, NULL),
-        CreateWindow("BUTTON", "Tarnthong Place Apartment", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 50, 200, 250, 40, hwnd, (HMENU)ID_BTN_TARNTHONG, hInstance, NULL),
-        CreateWindow("BUTTON", "Thongtara Monte", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 320, 200, 180, 40, hwnd, (HMENU)ID_BTN_THONGTARA, hInstance, NULL),
-        CreateWindow("BUTTON", "Home", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 20, 400, 120, 40, hwnd, (HMENU)ID_BTN_HOME, hInstance, NULL),
-        CreateWindow("BUTTON", "Log Out", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 450, 400, 120, 40, hwnd, (HMENU)ID_BTN_LOGOUT, hInstance, NULL)
+    HWND btns[] = {
+        GetDlgItem(hwnd, ID_BTN_HISTORY),
+        GetDlgItem(hwnd, ID_BTN_WARISA),
+        GetDlgItem(hwnd, ID_BTN_PS),
+        GetDlgItem(hwnd, ID_BTN_BAAAN),
+        GetDlgItem(hwnd, ID_BTN_TARNTHONG),
+        GetDlgItem(hwnd, ID_BTN_THONGTARA)
     };
     
-    for (int i = 0; i < 8; i++) {
-        SendMessage(buttons[i], WM_SETFONT, (WPARAM)hFont, TRUE);
+    for (int i = 0; i < 6; i++) {
+        MoveWindow(btns[i], i * buttonWidth, yOffset, buttonWidth, buttonHeight, TRUE);
     }
     
-    ShowWindow(hwnd, nCmdShow);
-    
-    MSG msg = {};
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-    
-    DeleteObject(hFont);
-    return 0;
+    MoveWindow(GetDlgItem(hwnd, ID_BTN_HOME), 50, height - 70, 150, 50, TRUE);
+    MoveWindow(GetDlgItem(hwnd, ID_BTN_LOGOUT), width - 200, height - 70, 150, 50, TRUE);
 }
